68894b8540f6ca5a82488f50278a50eaad9a5ccf
Sk Shahnawaz-ul Haque <shahnawaz-ul.haque@quantuminventions.com>
2022-08-05 11:17:55 +0530

    fix: Change text message

--- ASP.NET-Core.WebAPI/Utilities/AppResources.cs
+++ ASP.NET-Core.WebAPI/Utilities/AppResources.cs
@@ -7,12 +7,12 @@ internal static class AppResources
     internal const string MimeTypeApplicationJsonPatchJson = "application/json-patch+json";
     internal const string InternalServerErrorMessage = "Internal Server Error, please try again later.";
     internal const string BadRequestDueToAutoMapFailure = "Bad Request.";
-    internal const string ForbidRequestDueToUniqueConstraintFailure = "Duplicate item! {0} with value '{1}' already exists in Database.";
+    internal const string ForbidRequestDueToUniqueConstraintFailure = "Duplicate item! {0} with value '{1}' already exists in datastore.";
     internal const string ActionExceptionMessage = "Error occured in application, details : {@data}";
     internal const string InvalidValueValidationMessage = "Value of {0} is invalid.";
     internal const string EmptyValueValidationMessage = "Value of {0} can not be empty.";
     internal const string NoItemsFoundMessage = "No {0} found in Database.";
-    internal const string NoItemFoundMessage = "{0} with {1}: {2} not found in Database.";
+    internal const string NoItemFoundMessage = "{0} with {1}: {2} not found in datastore.";
     internal const string ModelValidationErrorMessage = "One or more validation errors occurred.";
     internal const string UnprocessableEntity = "Unprocessable entity.";
     internal const string InMemoryDbNameMissing = "Please provide a valid DB name is appsetting.json file.";
